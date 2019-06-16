c6155d866ee6802327e28d1d7247a3dcaa8418ac
usertersakiti <51637256+usertersakiti@users.noreply.github.com>
2019-06-16 18:32:04 +0700

    Add files via upload

---
+++ enkripsi.sh
@@ -0,0 +1,42 @@
+
+
+
+
+#Gitu Aja Rekode
+
+#Gimana Mau Maju Coba?
+
+
+#User_Tersakiti
+
+
+
+
+
+
+figlet -f future " Tools Enkripsi"
+echo ""
+echo "<|======================================|>"
+echo "Powerd By User_Tersakiti"
+echo "Indonesia Coder Of Hummanity"
+echo "<|======================================|>"
+sleep 2
+echo "Installling..."
+sleep 3
+pkg insall nodejs -y
+npm install -g bash-obfuscate
+
+mv enkripsi.sh ~
+
+clear
+echo "<|======================================|>"
+echo "Masukan Sc Yg May Di Enskripsi"
+read sc
+echo "<|======================================|>"
+echo "Masukan Nama Baru Bash nya "
+read acep
+bash-obfuscate $sc -o $acep
+echo "<|======================================|>"
+echo "Sc Telah Di Lock Dengan Nama " $acep
+echo "<|======================================|>"
+#User Tersakiti
